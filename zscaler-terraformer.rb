cb6bfd556493b9fc0733b52e48f7ec315c0a2d53
GitHub Terraform <zscaler-github-P@zscaler.com>
2022-09-26 16:24:42 -0700

    Update

--- zscaler-terraformer.rb
+++ zscaler-terraformer.rb
@@ -1,5 +1,5 @@
 class ZscalerTerraformer < Formula
-  desc "A command-line utility to terraforming your existing ZIA and ZPA resources"
+  desc "Command-line utility to terraforming your existing ZIA and ZPA resources"
   homepage "https://github.com/zscaler/zscaler-terraformer"
   url "https://github.com/zscaler/zscaler-terraformer/archive/refs/tags/v0.0.1.tar.gz"
   sha256 "f4cdf1adf85e01f2ff5ebb7c0dd3a5084a1be3552a459593f61a9013ca4ff5e4"
