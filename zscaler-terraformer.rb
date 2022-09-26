c9eb3fdaef5fc85cadb8aa391c225715f580fefe
GitHub Terraform <zscaler-github-P@zscaler.com>
2022-09-26 15:16:01 -0700

    Update

---
+++ zscaler-terraformer.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class ZscalerTerraformer < Formula
+    desc "A command line utility to facilitate terraforming your existing ZIA and ZPA resources."
+    homepage "https://github.com/zscaler/zscaler-terraformer"
+    url "https://github.com/zscaler/zscaler-terraformer/archive/refs/tags/v0.0.1.tar.gz"
+    sha256 "f4cdf1adf85e01f2ff5ebb7c0dd3a5084a1be3552a459593f61a9013ca4ff5e4"
+    license "MIT"
+
+    # depends_on "cmake" => :build
+
+    def install
+      # ENV.deparallelize  # if your formula fails when building in parallel
+      # Remove unrecognized options if warned by configure
+      # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+      system "./configure", *std_configure_args, "--disable-silent-rules"
+      # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    end
+
+    test do
+      # `test do` will create, run in and delete a temporary directory.
+      #
+      # This test will fail and we won't accept that! For Homebrew/homebrew-core
+      # this will need to be a test that verifies the functionality of the
+      # software. Run the test with `brew test zscaler-terraformer`. Options passed
+      # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+      #
+      # The installed folder is not in the path, so use the entire path to any
+      # executables being tested: `system "#{bin}/program", "do", "something"`.
+      system "false"
+    end
+  end
