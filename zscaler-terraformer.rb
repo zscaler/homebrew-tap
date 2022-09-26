03b8ba18124c4bd3205cc523f106335b7f8b2506
GitHub Terraform <zscaler-github-P@zscaler.com>
2022-09-26 14:11:36 -0700

    Update sha256 hash

--- zscaler-terraformer.rb
+++ zscaler-terraformer.rb
@@ -5,7 +5,7 @@ class ZscalerTerraformer < Formula
   desc "A command line utility to facilitate terraforming your existing ZIA and ZPA resources."
   homepage "https://github.com/zscaler"
   url "https://github.com/zscaler/zscaler-terraformer/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "f7a8be5f4d88e0959a4541ae7af15da777af08dc9b61ad787f6fa8c19d82e709"
+  sha256 "f4cdf1adf85e01f2ff5ebb7c0dd3a5084a1be3552a459593f61a9013ca4ff5e4"
   license "MIT"
 
   def install
