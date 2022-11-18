bbe5264697dac587072a9de2b1c90e036fdc6959
GitHub Terraform <zscaler-github-P@zscaler.com>
2022-11-18 09:39:47 -0800

    Released new zscaler-terraformer v0.0.1

--- formula/zscaler-terraformer.rb
+++ formula/zscaler-terraformer.rb
@@ -5,27 +5,27 @@ class ZscalerTerraformer < Formula
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/zscaler/zscaler-terraformer/releases/download/v#{version}/zscaler-terraformer_#{version}_darwin_amd64.zip"
-    sha256 "b48b1ad6aa34e9aad48a09375980b8b5d3dd9c4e30abc42954f0cfc9d5351763"
+    sha256 "87c62359f01583a0b10f14b0d20b3e14a55ded88748b7fa73cef8ef40c86a7f9"
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/zscaler/zscaler-terraformer/releases/download/v#{version}/zscaler-terraformer_#{version}_darwin_arm64.zip"
-    sha256 "d40df55c77eb5b8dbed3555d96fd8f29382b0de17f1cf4318e48daf6c985b158"
+    sha256 "b1e518eb7bac8c2f03af49a935c17d82a8c0f91773c34fc29f9f45dfedaee603"
   end
 
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/zscaler/zscaler-terraformer/releases/download/v#{version}/zscaler-terraformer_#{version}_linux_amd64.zip"
-    sha256 "602556f217c733e04563a232dd9da6c4f3773e0f93db180946b84a665993c957"
+    sha256 "1dc759d8e2882beaa78777add5a1fa35eac6509f387f1b41e199ed5720192b49"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
     url "https://github.com/zscaler/zscaler-terraformer/releases/download/v#{version}/zscaler-terraformer_#{version}_linux_arm.zip"
-    sha256 "99706c064cb79f37e16ffcb1fa313eb532dc2c2f2d5d367b13cf180450c97e57"
+    sha256 "d895d13af652daa660f8cf860f0964811d22ac406ab83e3f0a98c4de6e7e7cae"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
     url "https://github.com/zscaler/zscaler-terraformer/releases/download/v#{version}/zscaler-terraformer_#{version}_linux_arm64.zip"
-    sha256 "d80eeb835bb28d95746f31f9bf6c9f07706f502d11db8cedd8ab558d05ad2f40"
+    sha256 "e2940322fa652d28e2e8561c9342e5b6644748cc10dda62961fc55cb7ac33ccf"
   end
 
   conflicts_with "zscaler-terraformer"
