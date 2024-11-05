2345ce706bcecfb7b2e0215cc6c5e8def0fd0bf5
William Guilherme <wguilherme@securitygeek.io>
2024-11-04 18:02:42 -0800

    fix: Release v1.3.4

--- Casks/zscaler-terraformer.rb
+++ Casks/zscaler-terraformer.rb
@@ -5,7 +5,7 @@ cask "zscaler-terraformer" do
   name "zscaler-terraformer"
   homepage "https://github.com/zscaler/zscaler-terraformer"
   desc "CLI tool to generate terraform files from existing ZPA and ZIA"
-  version "1.3.3"
+  version "1.3.4"
 
   livecheck do
     url "https://github.com/zscaler/zscaler-terraformer/releases.atom"
@@ -15,27 +15,27 @@ cask "zscaler-terraformer" do
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/zscaler/zscaler-terraformer/releases/download/v#{version}/zscaler-terraformer_#{version}_darwin_amd64.zip"
-    sha256 "2485c93928fdefea291dd33f5067f28e269c28aecd833c4ffbf043f20d80ecb0"
+    sha256 "79791fed99e9de8e1037c960b7172e608704f1500e71120edd525bf2b572ffd9"
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/zscaler/zscaler-terraformer/releases/download/v#{version}/zscaler-terraformer_#{version}_darwin_arm64.zip"
-    sha256 "36aa5f8090867ce444b0aa458b2d9ef91ee61a7c172f184df29d1fbd1e690d7e"
+    sha256 "d5e00bd420f355b6da108748623a0f7a43f0f4b4782bd5d0dfb3bb7a8d44dba3"
   end
 
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/zscaler/zscaler-terraformer/releases/download/v#{version}/zscaler-terraformer_#{version}_linux_amd64.zip"
-    sha256 "814bce6c1e27565bf5ce1d88b31499f0d567424483c876311b642aeb523cfc9b"
+    sha256 "f035935b0a72d6cca4e0cc939c18b49c5a5e182ad0a59f6230b8318f9bd5cfa0"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
     url "https://github.com/zscaler/zscaler-terraformer/releases/download/v#{version}/zscaler-terraformer_#{version}_linux_arm.zip"
-    sha256 "f6e34bbff7354da2e145d6d7a936dc16a5172b2cbba3034722b04a2c4aad5e4a"
+    sha256 "4f202f817988f81c603cdd9dbe0923a11210522436ae840438169cf897355a4d"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
     url "https://github.com/zscaler/zscaler-terraformer/releases/download/v#{version}/zscaler-terraformer_#{version}_linux_arm64.zip"
-    sha256 "fe632b344d29ac42bbba41acc069308a8c0788f6bf6425cb90c495f287e983eb"
+    sha256 "f061f8c71e10b9c24feb36026ea515b52789c687e5ec31d397adb4f221631ff6"
   end
 
   binary "zscaler-terraformer"
