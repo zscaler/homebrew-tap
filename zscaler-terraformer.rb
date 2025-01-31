4c64232382e292ef697a4bf0e39a79eb2618e61c
William Guilherme <wguilherme@securitygeek.io>
2025-01-31 12:47:27 -0800

    fix: Release v2.0.1

--- zscaler-terraformer.rb
+++ zscaler-terraformer.rb
@@ -5,27 +5,27 @@ class ZscalerTerraformer < Formula
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/zscaler/zscaler-terraformer/releases/download/v#{version}/zscaler-terraformer_#{version}_darwin_amd64.zip"
-    sha256 "e777e2d624ffe5c8d956297d4adfd94606358010a57d0d26e136bcd488b0b2e4"
+    sha256 "7f1847402f4bdd964522af6c8953d9b79d138e6dddf50179533df42eba400f1c"
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/zscaler/zscaler-terraformer/releases/download/v#{version}/zscaler-terraformer_#{version}_darwin_arm64.zip"
-    sha256 "df28e6e0ca9576893b7cf8bc577d4f469c82c7581384c695ebc214cd0c6ca154"
+    sha256 "4960487b71c8d4d7624f2f4058cfb63b312cd2fa6e87013980900360dd021e84"
   end
 
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/zscaler/zscaler-terraformer/releases/download/v#{version}/zscaler-terraformer_#{version}_linux_amd64.zip"
-    sha256 "31c430284a68b22a50740a902451cc71528609fd098704e524e14a804d8f72f8"
+    sha256 "328f4df8163b34fe1d49f9c5dcf95d621732815ac2479e92ee5d70f07c67206f"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
     url "https://github.com/zscaler/zscaler-terraformer/releases/download/v#{version}/zscaler-terraformer_#{version}_linux_arm.zip"
-    sha256 "3b9ac2cfa36a22a1356b82e6e6daea8e8158a77a95447fdf8eb795c952e15266"
+    sha256 "e38d140b420770ea7082f2ea13ede8ae439eb41da3a8d7763e7b01c1365fa876"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
     url "https://github.com/zscaler/zscaler-terraformer/releases/download/v#{version}/zscaler-terraformer_#{version}_linux_arm64.zip"
-    sha256 "d83ef4f2c4efa494ded8ae0b7bdb39c6f760f8e4c6dc7869b1ee762334f22e97"
+    sha256 "325a35eeed47ee141f24bc1663b41b70f3cbd4970d49c6b785da5b958e56073c"
   end
 
   conflicts_with "zscaler-terraformer"
