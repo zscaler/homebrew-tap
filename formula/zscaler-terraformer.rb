0cc5f16dad18651340e0681b3c4fb41f3c72308d
William Guilherme <wguilherme@securitygeek.io>
2025-08-19 02:23:16 -0700

    fix: Release v2.0.13

--- formula/zscaler-terraformer.rb
+++ formula/zscaler-terraformer.rb
@@ -5,27 +5,27 @@ class ZscalerTerraformer < Formula
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/zscaler/zscaler-terraformer/releases/download/v#{version}/zscaler-terraformer_#{version}_darwin_amd64.zip"
-    sha256 "32a111346a27a3be6fae5a3396e59737baf8cac8bcc550108da68f272acac8cc"
+    sha256 "6d4029bafe3cab847c12884e4957195c39bd7576c885453a23d3516f11150808"
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/zscaler/zscaler-terraformer/releases/download/v#{version}/zscaler-terraformer_#{version}_darwin_arm64.zip"
-    sha256 "0e87e10a5a8086c877a21d489b4a8501f5f3ebaa0c19659766482d16ec2f9736"
+    sha256 "b2aee300b37a04f195434bba7796fd70ab3d2b4d9e84781331fe35af5e690bb9"
   end
 
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/zscaler/zscaler-terraformer/releases/download/v#{version}/zscaler-terraformer_#{version}_linux_amd64.zip"
-    sha256 "3d1de1a99c0c2cd4dadab4a3bf691de7f120fa46635a556fbb77d2329f24012c"
+    sha256 "6a9f64b4bbc70a23267970b8fb63bc0ba6d795d84b142d49c2822579faba805a"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
     url "https://github.com/zscaler/zscaler-terraformer/releases/download/v#{version}/zscaler-terraformer_#{version}_linux_arm.zip"
-    sha256 "e89223587cbccaaa49986169f1482d1ea58fe1cfaabe2b982e00d16f2f5bb26f"
+    sha256 "b4ec0b2c12771a0231e51d6310ca37547f5c83ebd6e750bb54277723ebdc18b7"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
     url "https://github.com/zscaler/zscaler-terraformer/releases/download/v#{version}/zscaler-terraformer_#{version}_linux_arm64.zip"
-    sha256 "0576a13b9d3324cd70d71de2d83dcec8e0a3ab9565b83a2c9b72ce8b141d4e54"
+    sha256 "89231f5734ea3b898811128e69e9a407441289b97f61eb3e6db2432a207f59f7"
   end
 
   conflicts_with "zscaler-terraformer"
